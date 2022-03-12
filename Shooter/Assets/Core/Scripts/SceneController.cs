148952d7dc10d80717e24c3fafd50bb1f2cd4b94
SamMarchenko <sam.marchenko@gmail.com>
2022-03-12 11:04:49 +0300

    Update Scene Controller

--- Shooter/Assets/Core/Scripts/SceneController.cs
+++ Shooter/Assets/Core/Scripts/SceneController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Enemies.Scripts;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using Random = UnityEngine.Random;
 
 public class SceneController : MonoBehaviour
@@ -23,15 +24,7 @@ public class SceneController : MonoBehaviour
     {
         if (_enemy == null)
         {
-            // Метод, копирующий объект - шаблон.
-            _enemy = Instantiate(_enemyPrefab) as GameObject;
-            //ПОДУМАТЬ, КАК УБРАТЬ GetComponent.
-            _Enemy.EnemyData = _enemies[Random.Range(0, _enemies.Length)];
-            float SpawnX = Random.Range(-23,23);
-            float SpawnZ = Random.Range(-30, 15);
-            _enemy.transform.position = new Vector3(SpawnX, 4, SpawnZ);
-            float angle = Random.Range(0, 360);
-            _enemy.transform.Rotate(0, angle, 0);
+            CreateEnemy();
         }
     }
 
