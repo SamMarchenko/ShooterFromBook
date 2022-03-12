8b5f0331658c97433beaea850a80159557e6b6d0
SamMarchenko <sam.marchenko@gmail.com>
2022-03-12 10:46:58 +0300

    Add UI Player HP

--- Shooter/Assets/Players/PlayerCharacter.cs
+++ Shooter/Assets/Players/PlayerCharacter.cs
@@ -3,20 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
 
 public class PlayerCharacter : MonoBehaviour
 {
     [SerializeField] private Player PlayerData;
+    [SerializeField] private Text PlHPLabel;
     private int _playerMaxHP;
 
     private void Start()
     {
         _playerMaxHP = PlayerData.PlayerHp;
+        PlHPLabel.text = $"<color=green>HP: {PlayerData.PlayerHp}</color>";
     }
 
     private void Update()
     {
-        if (PlayerData.PlayerHp <= 0)
+        if (PlayerData.PlayerHp == 0)
         {
             Destroy(this.gameObject);
             PlayerData.PlayerHp = _playerMaxHP;
@@ -26,7 +29,7 @@ public class PlayerCharacter : MonoBehaviour
     public void Hurt(int damage)
     {
         // Уменьшение здоровья игрока.
-        PlayerData.PlayerHp -= damage;
-        Debug.Log("Health = " + PlayerData.PlayerHp);
+        PlayerData.PlayerHp = (PlayerData.PlayerHp - damage) < 0 ? 0 : (PlayerData.PlayerHp - damage);
+        PlHPLabel.text = $"<color=green>HP: {PlayerData.PlayerHp}</color>";
     }
 }
