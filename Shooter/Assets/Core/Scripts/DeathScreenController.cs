066bfb97f3eb8e15549b664c40758466514f3f1b
SamMarchenko <sam.marchenko@gmail.com>
2022-03-12 21:17:06 +0300

    Fix shooting after death, add restart scene after death

--- Shooter/Assets/Core/Scripts/DeathScreenController.cs
+++ Shooter/Assets/Core/Scripts/DeathScreenController.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathScreenController : MonoBehaviour
-{  [SerializeField] private GameObject PlayerObject;
+{  
+    [SerializeField] private GameObject PlayerObject;
     private PlayerCharacter PlayerHP;
     [SerializeField] private Camera CameraObject;
     private MouseLook observY;
+    private RayShooter Shoot;
+    private Scene currentScene;
 
     [SerializeField] private GameObject DeathScreen;
     // Start is called before the first frame update
     void Start()
     {
+        currentScene = SceneManager.GetActiveScene();
         observY = CameraObject.GetComponent<MouseLook>();
         observY.enabled = true;
+
+        Shoot = CameraObject.GetComponent<RayShooter>();
+        Shoot.enabled = true;
 
         PlayerHP = PlayerObject.GetComponent<PlayerCharacter>();
         DeathScreen.SetActive(false);
@@ -24,7 +32,13 @@ public class DeathScreenController : MonoBehaviour
         if (PlayerHP.CurrentHP == 0)
         {
             observY.enabled = false;
+            Shoot.enabled = false;
             DeathScreen.SetActive(true);
+            if (Input.GetKeyUp(KeyCode.F))
+            {
+                SceneManager.LoadScene(currentScene.buildIndex, LoadSceneMode.Single);
+                Debug.Log("F clicked");
+            }
         }
     }
 }
