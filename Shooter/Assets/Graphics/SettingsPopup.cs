43dae5ed4679a474a708609ab1719b0cd5ab0956
SamMarchenko <sam.marchenko@gmail.com>
2022-03-20 19:26:32 +0300

    Added "Input Field" and "Slider" in "Settings Window". Update SettingsPopup script.

--- Shooter/Assets/Graphics/SettingsPopup.cs
+++ Shooter/Assets/Graphics/SettingsPopup.cs
@@ -1,9 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsPopup : MonoBehaviour
 {
+  [SerializeField] private Slider _Slider;
+
+  private void Start()
+  {
+    _Slider.value = PlayerPrefs.GetFloat("speed", 1);
+  }
+
   public void Open()
   {
     gameObject.SetActive(true);
@@ -12,5 +21,16 @@ public class SettingsPopup : MonoBehaviour
   public void Close()
   {
     gameObject.SetActive(false);
+  }
+
+  public void OnSubmitName(string name)
+  {
+    Debug.Log(name);
+  }
+
+  public void OnSpeedValue(float speed)
+  {
+    Debug.Log("Speed: " + speed);
+    PlayerPrefs.SetFloat("speed", speed);
   }
 }
